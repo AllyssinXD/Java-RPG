fcb31ef107402b9b9b83577a0020d99db720a1bd
alisson <alissonbrg32@gmail.com>
2025-01-25 20:32:37 -0300

    AAaaa

--- src/main/java/org/allyssinxd/zerodrpg/Game.java
+++ src/main/java/org/allyssinxd/zerodrpg/Game.java
@@ -3,19 +3,40 @@ package org.allyssinxd.zerodrpg;
 import org.allyssinxd.zerodrpg.entity.Entity;
 import org.allyssinxd.zerodrpg.utils.TextUtils;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
 
 
-    public static void NewGame(){
+    public static void NewGame()
+    {
         TextUtils.typeText("Criando novo jogo...", 100);
+
+        boolean cleanSlot = false;
+        int i = 0;
+        while (!cleanSlot) {
+            File gameSaveFile = new File("./saves/slot"+i);
+            if(!gameSaveFile.exists()) cleanSlot = true;
+            i++;
+        }
+
+        try{
+            File gameSaveFile = new File("./saves/slot"+i);
+            gameSaveFile.createNewFile();
+            Scanner scanner = new Scanner(gameSaveFile);
+            while(scanner.hasNextLine()){
+                System.out.println(scanner.nextLine());
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void LoadGame(){
-        System.out.println("---------- Carregando jogo. ----------");
+        TextUtils.typeText("Carregando jogo...", 100);
     }
 
     private int slot;
-
 }
